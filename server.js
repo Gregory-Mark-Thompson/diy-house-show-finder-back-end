26eeb5736a1a6b97efa51ff17a81d9cc4ebb58f1
generalGreg <thompson.gregory.mark@gmail.com>
2025-10-06 13:51:09 -0700

    Added process.env.PORT||3000

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use('/gigs', gigsRouter);
 app.use('/bands', bandsRouter);
 
 // Start the server and listen on port 3000
-app.listen(3000, () => {
+app.listen(process.env.PORT||3000, () => {
   console.log('The express app is ready!');
 });
